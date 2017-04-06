e5b2af0574d2084a016deef952db73bca5dda4db
Hurricane <ivsall2012@gmail.com>
2017-04-05 22:28:04 -0700

    can select buttons when touches get close

--- AHPinterest/AHShareModalVC.swift
+++ AHPinterest/AHShareModalVC.swift
@@ -8,31 +8,199 @@
 
 import UIKit
 
-//enum WhichSide {
-//    case leftSide
-//    case rightSide
-//    init(point: CGPoint, rect: CGRect) {
-//        if isLeft(point: point, rect: rect){
-//            self = .leftSide
-//        }else{
-//            self = .rightSide
-//        }
-//    }
-//    /// Return true if it's near to the left side of the view, false is for right side
-//    func isLeft(point: CGPoint, rect: CGRect) -> Bool {
-//        let size = rect.size
-//        if abs(point.x) < abs(size.width - point.x) {
-//            return true
-//        }else{
-//            return false
-//        }
-//    }
-//}
-
 fileprivate let minimumRingFollowDistance: CGFloat = 100.0
 fileprivate let radius: CGFloat = 80.0
 fileprivate let btnSize: CGSize = CGSize(width: 55, height: 55)
 fileprivate let fingerRingSize:CGSize = CGSize(width: 60, height: 60)
+
+
+class AHShareModalVC: UIViewController {
+    let btnLeft = UIButton(type: .custom)
+    let btnMiddle = UIButton(type: .custom)
+    let btnRight = UIButton(type: .custom)
+    var fingerRingView = UIImageView(image: #imageLiteral(resourceName: "finger-ring"))
+    var allButtons: [UIButton]?
+    
+    var buttonsPositions: ButtonsPositions? = nil {
+        didSet {
+            if buttonsPositions != nil {
+                btnLeft.center = startingPoint!
+                btnMiddle.center = startingPoint!
+                btnRight.center = startingPoint!
+                
+                
+            }
+        }
+    }
+    var startingPoint: CGPoint? {
+        didSet {
+            if let startingPoint = startingPoint {
+                buttonsPositions = ButtonsPositions(anchor: startingPoint, radius: radius)
+                buttonsPositions?.decide()
+                fingerRingView.center = startingPoint
+                fingerRingView.bounds.size = fingerRingSize
+                self.view.addSubview(fingerRingView)
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupButtons()
+    }
+    func setupButtons() {
+        btnLeft.setImage(#imageLiteral(resourceName: "pin-btn-normal"), for: .normal)
+        btnLeft.setImage(#imageLiteral(resourceName: "pin-btn-selected"), for: .selected)
+        btnLeft.bounds.size = btnSize
+        btnLeft.addTarget(self, action: #selector(pinBtnTapped(_:)), for: .touchUpInside)
+        btnMiddle.setImage(#imageLiteral(resourceName: "paper-plane-normal"), for: .normal)
+        btnMiddle.setImage(#imageLiteral(resourceName: "paper-plane-selected"), for: .selected)
+        btnMiddle.bounds.size = btnSize
+        btnMiddle.addTarget(self, action: #selector(papaerPlaneTapped(_:)), for: .touchUpInside)
+        btnRight.setImage(#imageLiteral(resourceName: "more-btn-normal"), for: .normal)
+        btnRight.setImage(#imageLiteral(resourceName: "more-btn-selected"), for: .selected)
+        btnRight.bounds.size = btnSize
+        btnRight.addTarget(self, action: #selector(moreBtnPlaneTapped(_:)), for: .touchUpInside)
+        
+        allButtons = [btnLeft,btnMiddle,btnRight]
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        btnLeft.alpha = 0.0
+        btnMiddle.alpha = 0.0
+        btnRight.alpha = 0.0
+        self.view.addSubview(btnLeft)
+        self.view.addSubview(btnMiddle)
+        self.view.addSubview(btnRight)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    /// This function is called only by the transition animator
+    func buttonAnimation() {
+        if let buttonsPositions = buttonsPositions {
+            UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.0, options: [], animations: {
+                self.btnLeft.center = buttonsPositions.left
+                self.btnMiddle.center = buttonsPositions.middle
+                self.btnRight.center = buttonsPositions.right
+                self.btnLeft.alpha = 1.0
+                self.btnMiddle.alpha = 1.0
+                self.btnRight.alpha = 1.0
+                }, completion: nil)
+        }
+    }
+}
+
+///MARK:- Handler Events
+extension AHShareModalVC {
+    func pinBtnTapped(_ sender: UIButton){
+        print("pinBtnTapped")
+        dismiss(animated: true, completion: nil)
+    }
+    func papaerPlaneTapped(_ sender: UIButton){
+        print("papaerPlaneTapped")
+        dismiss(animated: true, completion: nil)
+    }
+    func moreBtnPlaneTapped(_ sender: UIButton){
+        print("moreBtnPlaneTapped")
+        dismiss(animated: true, completion: nil)
+    }
+
+}
+
+///MARK:- Hooks for longPress
+extension AHShareModalVC {
+    func changed(point: CGPoint){
+        guard let startingPoint = startingPoint else {
+            return
+        }
+        fingerRingAnimation(point, startingPoint)
+        buttonsAnimation(point, startingPoint)
+        
+    }
+    
+    
+    func ended(point: CGPoint) {
+        guard let allButtons = allButtons else {
+            return
+        }
+        var targetBtn: UIButton? = nil
+        for btn in allButtons {
+            if btn.isSelected {
+                targetBtn = btn
+                btn.sendActions(for: .touchUpInside)
+            }
+        }
+        if targetBtn == nil {
+            btnLeft.removeFromSuperview()
+            btnMiddle.removeFromSuperview()
+            btnRight.removeFromSuperview()
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+}
+
+
+///MARK:- Helper Functions
+extension AHShareModalVC {
+    func buttonsAnimation(_ point: CGPoint, _ startingPoint: CGPoint) {
+        guard let allButtons = allButtons else {
+            return
+        }
+        
+        let trigerDistance = radius * 0.7
+        var min: CGFloat = CGFloat(FLT_MAX)
+        var targetBtn: UIButton? = nil
+        for btn in allButtons {
+            let delta = calculateDistance(pointA: point, PointB: btn.center)
+            if delta < min && delta <= trigerDistance {
+                min = delta
+                targetBtn = btn
+            }
+            btn.isSelected = false
+        }
+        
+        if targetBtn != nil {
+            targetBtn!.isSelected = true
+        }
+        
+        
+        
+    }
+
+    func fingerRingAnimation(_ point: CGPoint, _ startingPoint: CGPoint) {
+        let distanceToStarting = calculateDistance(pointA: point, PointB: startingPoint)
+        if distanceToStarting > minimumRingFollowDistance {
+            let ringToStarting = calculateDistance(pointA: fingerRingView.center, PointB: startingPoint)
+            
+            if ringToStarting > CGFloat(2.0) {
+                // case 1, fingerRingView position was following the touch, but now it goes too far, it needs to go back to startingPoint
+                ringGoesBack()
+            }else{
+                // case 2, current fingerRingView position is at(near) startingPoint, return
+                return
+            }
+            
+        }else{
+            // the point is within the distance, follow the touch
+            fingerRingView.center = point
+        }
+    }
+    func ringGoesBack() {
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [], animations: {
+            self.fingerRingView.center = self.startingPoint!
+            }, completion: nil)
+    }
+    
+    func calculateDistance(pointA: CGPoint, PointB: CGPoint) -> CGFloat {
+        let xDist = pointA.x - PointB.x
+        let yDist = pointA.y - PointB.y
+        return CGFloat(sqrt((xDist * xDist) + (yDist * yDist)))
+    }
+}
+
 
 
 
@@ -42,8 +210,6 @@ struct ButtonsPositions {
     var right:CGPoint = .zero
     var anchor: CGPoint = .zero
     var radius: CGFloat = 0.0
-//    var whichSide: WhichSide?
-//    var restraint:CGRect = .zero
     init(anchor: CGPoint, radius:CGFloat) {
         self.anchor = anchor
         self.radius = radius
@@ -65,7 +231,7 @@ struct ButtonsPositions {
             zero_pi_up()
         }
         else if (anchor.x <= 6 * unit && anchor.x > 5 * unit) ||
-                (anchor.x < 2 * unit && anchor.x >= 1 * unit){
+            (anchor.x < 2 * unit && anchor.x >= 1 * unit){
             pi_radian_up_left()
         }
         else if (anchor.x < 7 * unit && anchor.x > 6 * unit) ||
@@ -95,7 +261,7 @@ struct ButtonsPositions {
         
         
     }
-
+    
     
     func flipBasedOnX(for point: CGPoint, basedOn anchor: CGPoint) -> CGPoint {
         let x = point.x
@@ -107,7 +273,7 @@ struct ButtonsPositions {
         let x = anchor.x + (anchor.x - point.x)
         return CGPoint(x: x, y: y)
     }
-
+    
 }
 
 ///MARK:- Helper functions for positioning the coordinates of the buttons
@@ -174,128 +340,6 @@ extension ButtonsPositions {
         
     }
 }
-
-
-
-class AHShareModalVC: UIViewController {
-    let btnLeft = UIButton(type: .custom)
-    let btnMiddle = UIButton(type: .custom)
-    let btnRight = UIButton(type: .custom)
-    var fingerRingView = UIImageView(image: #imageLiteral(resourceName: "finger-ring"))
-    
-    
-    var buttonsPositions: ButtonsPositions? = nil {
-        didSet {
-            if buttonsPositions != nil {
-                btnLeft.center = startingPoint!
-                btnMiddle.center = startingPoint!
-                btnRight.center = startingPoint!
-                
-                
-            }
-        }
-    }
-    var startingPoint: CGPoint? {
-        didSet {
-            if let startingPoint = startingPoint {
-                buttonsPositions = ButtonsPositions(anchor: startingPoint, radius: radius)
-                buttonsPositions?.decide()
-                fingerRingView.center = startingPoint
-                fingerRingView.bounds.size = fingerRingSize
-                self.view.addSubview(fingerRingView)
-                self.view.layoutIfNeeded()
-            }
-        }
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        btnLeft.setImage(#imageLiteral(resourceName: "pin-btn"), for: .normal)
-        btnLeft.bounds.size = btnSize
-        btnMiddle.setImage(#imageLiteral(resourceName: "paper-plane"), for: .normal)
-        btnMiddle.bounds.size = btnSize
-        btnRight.setImage(#imageLiteral(resourceName: "more-btn"), for: .normal)
-        btnRight.bounds.size = btnSize
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        btnLeft.alpha = 0.0
-        btnMiddle.alpha = 0.0
-        btnRight.alpha = 0.0
-        self.view.addSubview(btnLeft)
-        self.view.addSubview(btnMiddle)
-        self.view.addSubview(btnRight)
-    }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
-    
-    /// This function is called only by the transition animator
-    func buttonAnimation() {
-        if let buttonsPositions = buttonsPositions {
-            UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.0, options: [], animations: {
-                self.btnLeft.center = buttonsPositions.left
-                self.btnMiddle.center = buttonsPositions.middle
-                self.btnRight.center = buttonsPositions.right
-                self.btnLeft.alpha = 1.0
-                self.btnMiddle.alpha = 1.0
-                self.btnRight.alpha = 1.0
-                }, completion: nil)
-        }
-    }
-}
-
-///MARK:- Hooks for longPress
-extension AHShareModalVC {
-    func changed(point: CGPoint){
-        guard let startingPoint = startingPoint else {
-            return
-        }
-        let distanceToStarting = calculateDistance(pointA: point, PointB: startingPoint)
-        if distanceToStarting > minimumRingFollowDistance {
-            let ringToStarting = calculateDistance(pointA: fingerRingView.center, PointB: startingPoint)
-            
-            if ringToStarting > CGFloat(2.0) {
-                // case 1, fingerRingView position was following the touch, but now it goes too far, it needs to go back to startingPoint
-                ringGoesBack()
-            }else{
-                // case 2, current fingerRingView position is at(near) startingPoint, return
-                return
-            }
-            
-        }else{
-            // the point is within the distance, follow the touch
-            fingerRingView.center = point
-        }
-        
-    }
-    func ringGoesBack() {
-        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [], animations: { 
-            self.fingerRingView.center = self.startingPoint!
-            }, completion: nil)
-    }
-    func ended(point: CGPoint) {
-        btnLeft.removeFromSuperview()
-        btnMiddle.removeFromSuperview()
-        btnRight.removeFromSuperview()
-        dismiss(animated: true, completion: nil)
-    }
-    func calculateDistance(pointA: CGPoint, PointB: CGPoint) -> CGFloat {
-        let xDist = pointA.x - PointB.x
-        let yDist = pointA.y - PointB.y
-        return CGFloat(sqrt((xDist * xDist) + (yDist * yDist)))
-    }
-}
-
-
-///MARK:- Helper Functions
-extension AHShareModalVC {
-
-}
-
-
-
-
-
 
 
 
