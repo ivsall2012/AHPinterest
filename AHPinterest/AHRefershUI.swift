389798e1054d758b967ec4148a49ded3f757c5a1
Hurricane <ivsall2012@gmail.com>
2017-04-11 03:34:36 -0700

    add first time loading refresh animation

---
+++ AHPinterest/AHRefershUI.swift
@@ -0,0 +1,80 @@
+//
+//  AHRefershUI.swift
+//  AHPinterest
+//
+//  Created by Andy Hurricane on 4/11/17.
+//  Copyright © 2017 AndyHurricane. All rights reserved.
+//
+
+import UIKit
+
+class AHRefershUI: NSObject {
+    fileprivate static var isSetup = false
+    fileprivate static let screenSize: CGSize = UIScreen.main.bounds.size
+    fileprivate static var refreshControl: UIImageView = UIImageView(image: #imageLiteral(resourceName: "refresh-control"))
+    
+    fileprivate class func setup() {
+        guard let window = UIApplication.shared.keyWindow else {
+            return
+        }
+        window.addSubview(refreshControl)
+        refreshControl.frame.size = .init(width: 50, height: 50)
+        refreshControl.center = window.center
+    }
+    
+    class func show() {
+        if !isSetup {
+            setup()
+        }
+        refreshControl.isHidden = false
+        startAnimateRefresh()
+    }
+    
+    fileprivate class func startAnimateRefresh() {
+        print("A")
+        // need to do animation and networking
+        let rotaton = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotaton.fromValue = 0.0
+        rotaton.toValue = 2 * CGFloat(M_PI)
+        rotaton.duration = 1.0;
+        rotaton.repeatCount = FLT_MAX;
+        
+        let scale = CABasicAnimation(keyPath: "transform.scale")
+        scale.fromValue = 0.3
+        scale.toValue = 1.0
+        scale.duration = 0.5;
+        
+        refreshControl.layer.add(scale, forKey: "scale")
+        refreshControl.layer.add(rotaton, forKey: "ratate")
+        
+        
+    }
+    
+    fileprivate class func stopAnimateRefresh() {
+        refreshControl.layer.removeAllAnimations()
+        
+        let rotaton = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotaton.fromValue = 0.0
+        rotaton.toValue = 2 * CGFloat(M_PI)
+        rotaton.duration = 1.0;
+        rotaton.repeatCount = FLT_MAX;
+        
+        let scale = CABasicAnimation(keyPath: "transform.scale")
+        scale.fromValue = 1.0
+        scale.toValue = 0.0
+        scale.duration = 0.5;
+        
+        refreshControl.layer.add(scale, forKey: "scale")
+        refreshControl.layer.add(rotaton, forKey: "ratate")
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5 ) {
+            refreshControl.layer.removeAllAnimations()
+            refreshControl.isHidden = true
+        }
+        
+    }
+
+    class func dismiss() {
+        stopAnimateRefresh()
+    }
+}
--- AHPinterest/ViewController.swift
+++ AHPinterest/ViewController.swift
@@ -10,24 +10,28 @@ import UIKit
 
 class ViewController: UICollectionViewController {
     var viewModel: ViewModel?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let view = UIView(frame: .init(x: 0, y: -100, width: 376, height: 100))
-//        view.backgroundColor = UIColor.red
-//        collectionView?.addSubview(view)
         viewModel = ViewModel(collectionView: collectionView!, reusablePinCellID: "PinCell")
         viewModel?.mainVC = self
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        AHRefershUI.show()
         viewModel?.loadNewData(completion: { (success) in
+            AHRefershUI.dismiss()
             if success {
                 // dismiss refresh control
             }else{
                 // do something about it and dismiss refresh control too
             }
         })
-    }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        collectionView?.reloadData()
     }
 
 }
