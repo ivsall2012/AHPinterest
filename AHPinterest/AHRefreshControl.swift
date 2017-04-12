6dc3ad2e407ca949868f684b45f8d13d0fa0b682
Hurricane <ivsall2012@gmail.com>
2017-04-11 17:25:32 -0700

    small improvements

--- AHPinterest/AHRefreshControl.swift
+++ AHPinterest/AHRefreshControl.swift
@@ -16,23 +16,57 @@ class AHRefreshControl: NSObject {
 
 extension AHRefreshControl {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        handlerPullToRefresh(scrollView, didEndDragging: false)
+    }
+    
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        handlerPullToRefresh(scrollView, didEndDragging: true)
+        
+    }
+}
+
+
+// MARK:- For Pull-to-Refresh stuff
+extension AHRefreshControl {
+    func handlerPullToRefresh(_ scrollView: UIScrollView, didEndDragging: Bool) {
         guard let headerCell = headerCell else {
             return
         }
+        // if the refreshControl is spinning then return
         guard !headerCell.isSpinning else {
             return
         }
-        
-        let yOffset = scrollView.contentOffset.y
-        let topInset = scrollView.contentInset.top
-        // the following extra -10 is to make the refreshControl hide "faster"
-        if yOffset < -topInset - 10 {
-            headerCell.isHidden = false
-            showingRefreshControl(yOffset: abs(yOffset), headerCell: headerCell)
+        if didEndDragging {
+            // user lifted their touch, now check if refresh should be triggered or not
+            if headerCell.ratio >= AHHeaderShouldRefreshRatio {
+                headerCell.refresh()
+                UIView.animate(withDuration: 0.25, animations: {
+                    scrollView.contentInset.top = headerCell.bounds.height
+                    }, completion: { (_) in
+                        self.viewModel?.loadNewData(completion: { (_) in
+                            UIView.animate(withDuration: 0.25, animations: {
+                                scrollView.contentInset = AHCollectionViewInset
+                            })
+                        })
+                        
+                })
+            }
         }else{
-            headerCell.isHidden = true
+            // user is still dragging
+            let yOffset = scrollView.contentOffset.y
+            let topInset = scrollView.contentInset.top
+            // the following extra -10 is to make the refreshControl hide "faster"
+            if yOffset < -topInset - 10 {
+                headerCell.isHidden = false
+                showingRefreshControl(yOffset: abs(yOffset), headerCell: headerCell)
+            }else{
+                headerCell.isHidden = true
+            }
+
         }
     }
+    /// tell the header cell how much to pull down
     func showingRefreshControl(yOffset: CGFloat, headerCell: AHCollectionRefreshHeader) {
         guard yOffset >= 0.0 else {
             return
@@ -43,26 +77,6 @@ extension AHRefreshControl {
         }
     }
     
-    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        guard let headerCell = headerCell else {
-            return
-        }
-        guard !headerCell.isSpinning else {
-            return
-        }
-        if headerCell.ratio >= AHHeaderShouldRefreshRatio {
-            headerCell.refresh()
-            UIView.animate(withDuration: 0.25, animations: {
-                scrollView.contentInset.top = headerCell.bounds.height
-                }, completion: { (_) in
-                    self.viewModel?.loadNewData(completion: { (_) in
-                        UIView.animate(withDuration: 0.25, animations: { 
-                            scrollView.contentInset = AHCollectionViewInset
-                        })
-                    })
-
-            })
-        }
-        
-    }
 }
+
+
